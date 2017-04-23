f8472b0aaca205b0e9134b093e5ff6772c28b5da
yangdawn <ytx1993@outlook.com>
2017-04-23 22:57:13 +0800

    继续完善博客页面视图；完善django后台信息。

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post, Category, Tag
 
 class PostAdmin(admin.ModelAdmin):
-    list_diaplay = ['title', 'created_time', 'modified_time', 'category', 'author']
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
